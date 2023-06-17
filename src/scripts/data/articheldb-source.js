134d2d5579520e9fd66715168c9dc881e9829149
r4ha <31908934+rahadinabudiman@users.noreply.github.com>
2023-06-17 09:13:03 +0700

    Update: Merging

--- src/scripts/data/articheldb-source.js
+++ src/scripts/data/articheldb-source.js
@@ -12,6 +12,138 @@ class ArtichelDbSource {
     const responseJson = await response.json();
     return responseJson.data;
   }
+
+  static async getAdminArticles() {
+    const response = await fetch(API_ENDPOINT.ADMIN_ARTICLES);
+    const responseJson = await response.json();
+    return responseJson.data;
+  }
+
+  static async createArticle(formData, token) {
+    try {
+      const response = await fetch(API_ENDPOINT.CREATE_ARTICLE, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+      }
+
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async deleteArticle(articleId, token) {
+    try {
+      const response = await fetch(API_ENDPOINT.DELETE_ARTICLE(articleId), {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+      }
+
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async updateArticle(formData, articleId, token) {
+    try {
+      const response = await fetch(API_ENDPOINT.EDIT_ARTICLE(articleId), {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+      }
+
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async loginAdmin(username, password) {
+    try {
+      const data = {
+        username,
+        password,
+      };
+
+      const response = await fetch(API_ENDPOINT.LOGIN_ADMIN, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+      }
+
+      const responseData = await response.clone().json();
+      document.cookie = `bEDUCookie=${responseData.data.token}; path=/`;
+      console.log(responseData.message);
+      return responseData;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async registerAdmin(nama, username, email, password, passwordConfirm) {
+    try {
+      const data = {
+        nama,
+        username,
+        email,
+        password,
+        passwordConfirm,
+      };
+
+      console.log('Ini data', data);
+
+      const response = await fetch(API_ENDPOINT.REGISTER_ADMIN, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+      }
+
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default ArtichelDbSource;
