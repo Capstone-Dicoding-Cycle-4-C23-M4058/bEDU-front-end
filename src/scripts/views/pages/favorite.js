54ef16a4970ee1e382d78e7ebfb7d013b80fe7ff
r4ha <31908934+rahadinabudiman@users.noreply.github.com>
2023-06-18 00:19:05 +0700

    Update: Swal and Refactor Codes

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,5 +1,6 @@
 import FavoriteBookIdb from '../../data/favorite-idb';
 import { createTemplateArtichel } from '../templates/template-creator';
+import ArtichelDbSource from '../../data/articheldb-source';
 
 const Favorite = {
   async render() {
@@ -20,6 +21,7 @@ const Favorite = {
   },
 
   async afterRender() {
+    const bEDUCookie = getCookieValue('bEDUCookie');
     const List = await FavoriteBookIdb.getAllBook();
     const Container = document.querySelector('.post');
     const empty = document.getElementById('empty-content');
@@ -41,6 +43,55 @@ const Favorite = {
       });
       empty.innerHTML = '';
     }
+
+    const logoutUser = document.querySelectorAll('#nav-btn4');
+  logoutUser.forEach(button => {
+  button.addEventListener('click', async () => {
+    try {
+      // Cek isi innerHTML dari #nav-btn4
+      if (button.innerHTML === 'Logout') {
+        // Lakukan tindakan logout
+        const response = await ArtichelDbSource.logoutUser(bEDUCookie);
+        console.log(response);
+
+        const metaTag = document.createElement('meta');
+        metaTag.setAttribute('http-equiv', 'refresh');
+        metaTag.setAttribute('content', '1;/');
+        document.head.appendChild(metaTag);
+        // Tambahkan kode logout di sini
+      } else if (button.innerHTML === 'Login') {
+        // Redirect ke halaman login
+        const metaTag = document.createElement('meta');
+        metaTag.setAttribute('http-equiv', 'refresh');
+        metaTag.setAttribute('content', '1;/#/login_user');
+        document.head.appendChild(metaTag);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+function getCookieValue(cookieName) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(`${cookieName}=`)) {
+      return cookie.substring(cookieName.length + 1);
+    }
+  }
+  return null;
+}
+function parseJwtPayload(token) {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = atob(base64);
+    return JSON.parse(jsonPayload);
+  } catch (error) {
+    return null;
+  }
+}
   },
 };
 
