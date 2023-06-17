66f9fc5aa8b3e9e320b8ed5b7e67ee4da33e720f
unknown <malikasyraf30@gmail.com>
2023-06-17 11:36:00 +0700

    updateuser

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
 import '../styles/admin.css';
+import '../styles/user.css';
 import App from './views/app';
 
 const app = new App({
